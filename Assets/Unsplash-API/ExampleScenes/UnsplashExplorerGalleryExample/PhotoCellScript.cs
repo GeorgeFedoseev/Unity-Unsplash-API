e5345b4d13edb39c74c8bfa791a10982374c7723
George Fedoseev <george.fedoseev@me.com>
2019-04-11 14:56:29 +0300

    galery example

---
+++ Assets/Unsplash-API/ExampleScenes/UnsplashExplorerGalleryExample/PhotoCellScript.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using UnsplashExplorerForUnity;
+using UnsplashExplorerForUnity.Model;
+
+public class PhotoCellScript : MonoBehaviour
+{
+    [SerializeField]
+    private RawImage _rawImage;
+
+    [SerializeField]
+    private AspectRatioFitter _aspectRatioFitter;
+
+
+    [SerializeField]
+    private GameObject _loadingOverlay;
+    [SerializeField]
+    private Image _loadingProgressIndicator;
+
+    [SerializeField]
+    private GameObject _errorLoadingOverlay;
+    [SerializeField]
+    private Text _errorLoadingText;
+
+    private UnsplashDownloader _downloader;
+    private UnsplashPhoto _photo;
+    
+
+
+
+    public void InitWith(UnsplashPhoto photo){
+        _photo = photo;
+
+        ShowErrorLoading(false);
+        ShowRawImage(false);
+        ShowLoading(true);
+
+        _downloader = new UnsplashDownloader();
+        _downloader.DownloadPhotoAsync(_photo, new Progress<float>(OnDownloadProgress), UnsplashPhotoSize.Thumb)
+        .ContinueWith(t => {
+            if(t.IsCanceled){
+                ShowErrorLoading(true, "Loading Canceled");
+            }else if(t.IsFaulted){
+                ShowErrorLoading(true, "ERROR");
+                Debug.LogException(t.Exception);
+            }else{
+                SetTexture(t.Result);
+                ShowRawImage(true);
+            }
+
+            ShowLoading(false);            
+        }, TaskScheduler.FromCurrentSynchronizationContext()).LogExceptions();
+    }
+
+    private void ShowLoading(bool show){
+        _loadingOverlay.SetActive(show);
+        _loadingProgressIndicator.gameObject.SetActive(show);
+    }
+
+    private void ShowErrorLoading(bool show, string message = "Error"){
+        _errorLoadingOverlay.SetActive(show);
+        _errorLoadingText.text = message;
+    }
+
+    private void ShowRawImage(bool show){
+        _rawImage.gameObject.SetActive(show);
+    }
+
+    private void SetTexture(Texture2D tex){
+        if(_rawImage.texture != null){
+            Destroy(_rawImage.texture);
+        }
+        _rawImage.texture = tex;
+        _aspectRatioFitter.aspectRatio = (float)tex.width/tex.height;
+    }
+
+    // EVENTS
+    private void OnDownloadProgress(float progress){
+        _loadingProgressIndicator.fillAmount = progress;
+    }
+
+    private void OnDestroy(){
+        if(_rawImage.texture != null){
+            Destroy(_rawImage.texture);
+        }
+    }
+}
---
+++ Assets/Unsplash-API/ExampleScenes/UnsplashExplorerGalleryExample/UnsplashExplorerGalleryExampleScript.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using UnsplashExplorerForUnity;
+using UnsplashExplorerForUnity.Model;
+
+public class UnsplashExplorerGalleryExampleScript : MonoBehaviour
+{   
+
+    [SerializeField]
+    private InputField _searchInput;
+
+    [SerializeField]
+    private Transform _searchResultsContainer;
+    
+    [SerializeField]
+    private GameObject _photoCellPrefab;
+
+    private Coroutine _debounceCoroutine;
+
+    // Start is called before the first frame update
+    void Start() {
+        Application.targetFrameRate = 60;
+
+
+        ClearContainer(_searchResultsContainer);
+
+        _searchInput.Select();
+
+       _searchInput.onValueChanged.AddListener((val) => {
+           if(_debounceCoroutine != null){
+               StopCoroutine(_debounceCoroutine);
+           }
+           _debounceCoroutine = StartCoroutine(DebounceCoroutine());
+       });
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // METHODS
+    IEnumerator DebounceCoroutine(){
+        yield return new WaitForSeconds(0.5f);
+        LoadPhotosForQuery(_searchInput.text);
+
+        _debounceCoroutine = null;
+    }
+
+    private void LoadPhotosForQuery(string query, int page = 1){
+        if(string.IsNullOrWhiteSpace(query)){
+            ClearContainer(_searchResultsContainer);
+            return;
+        }
+
+        UnsplashExplorer.Main.SearchPhotos(query).ContinueWith(t => {
+            if(t.IsCanceled){
+                print("search query canceled");
+            }else if(t.IsFaulted){
+                Debug.LogError($"Search query failed: {t.Exception}");
+            }else{
+                AppendSearchResults(t.Result, page);
+            }
+        }, TaskScheduler.FromCurrentSynchronizationContext()).LogExceptions();
+    }
+
+    private void AppendSearchResults(List<UnsplashPhoto> photos, int page){
+        if(page == 1){
+            ClearContainer(_searchResultsContainer);
+        }
+
+        foreach(var photo in photos){
+            var cell = InstantiateIntoContainer<PhotoCellScript>(_photoCellPrefab, _searchResultsContainer);
+            cell.InitWith(photo);
+        }
+    }
+
+
+
+
+    // STATIC
+    public static void ClearContainer(Transform container){
+        foreach(Transform t in container){
+            if(Application.isPlaying){
+                GameObject.Destroy(t.gameObject);
+            }else{
+                GameObject.DestroyImmediate(t.gameObject);
+            }
+            
+        }
+    }   
+
+    public static T InstantiateIntoContainer<T>(UnityEngine.Object prefab, Transform container) where T: MonoBehaviour {
+        var a = (GameObject.Instantiate(prefab) as GameObject).GetComponent<T>();
+        a.transform.SetParent(container);
+        a.transform.localScale = Vector3.one;
+        a.transform.localPosition = Vector3.zero;
+
+        return a;
+    }
+}
